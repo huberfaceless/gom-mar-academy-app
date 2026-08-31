cefc9f4ba943303dae639954102c13b70955004d
gomo <huberfaceless@gmail.com>
2026-08-31 16:16:14 +0200

    Etappe 92: AcademyView.tsx aktualisieren

--- src/components/AcademyView.tsx
+++ src/components/AcademyView.tsx
@@ -16,6 +16,7 @@ import { localizeAcademyStage88 } from '../i18n/academyLocalization88';
 import { localizeAcademyStage89 } from '../i18n/academyLocalization89';
 import { localizeAcademyStage90 } from '../i18n/academyLocalization90';
 import { localizeAcademyStage91 } from '../i18n/academyLocalization91';
+import { localizeAcademyStage92 } from '../i18n/academyLocalization92';
 import { 
   Play, 
   CheckCircle, 
@@ -117,7 +118,7 @@ export const AcademyView: React.FC<AcademyViewProps> = ({
 }) => {
   const { language } = useLanguage();
   const copy = academyCopy[language];
-  const localizedStages = useMemo(() => localizeAcademyStage91(localizeAcademyStage90(localizeAcademyStage89(localizeAcademyStage88(localizeAcademyStage87(localizeAcademyStage86(localizeAcademyStage85(localizeAcademyStage84(localizeAcademyStage83(localizeAcademyStage82(localizeAcademyStage81(localizeAcademyStages(stages, language), language), language), language), language), language), language), language), language), language), language), language), [stages, language]);
+  const localizedStages = useMemo(() => localizeAcademyStage92(localizeAcademyStage91(localizeAcademyStage90(localizeAcademyStage89(localizeAcademyStage88(localizeAcademyStage87(localizeAcademyStage86(localizeAcademyStage85(localizeAcademyStage84(localizeAcademyStage83(localizeAcademyStage82(localizeAcademyStage81(localizeAcademyStages(stages, language), language), language), language), language), language), language), language), language), language), language), language), language), [stages, language]);
   const isLight = user.theme === 'clean-light' || !user.theme;
 
   // If an initial lesson ID was passed, open lesson mode directly; otherwise start in course overview
